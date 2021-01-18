29dfc7ddcb92ac4f78689e8af15bda4a7163a748
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2021-01-18 15:24:29 +0800

    就決定要放煙火了

---
+++ VR_End_107051046/Assets/scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;   //引用 場景管理器 API
+
+public class GameManager : MonoBehaviour
+{
+    /// <summary>
+    /// 重新遊戲
+    /// </summary>
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("VREND");
+    }
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitGame()
+    {
+        //應用程式.離開遊戲()
+        Application.Quit();
+    }
+
+}
