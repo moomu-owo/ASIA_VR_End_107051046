69df4ed3e044b7d5e0a9a149fa4372ae2906fc49
moomu-owo <71679979+moomu-owo@users.noreply.github.com>
2021-01-19 01:15:11 +0800

    就剩粒子系統打不開了

--- VR_End_107051046/Assets/scripts/FireManager.cs
+++ VR_End_107051046/Assets/scripts/FireManager.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class FireManager : MonoBehaviour
+
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("煙火音效")]
+    public AudioClip soundIn;
+    [Header("煙火特效")]
+    public ParticleSystem firework;
+
+    private AudioSource aud;
+    private ParticleSystem particleObject;
+
+    public bool fire;
+
+    private void Awake()
     {
-        
+        // 音效來源 = 取得元件<音效來源>()
+        aud = GetComponent<AudioSource>();
+        particleObject = GetComponent<ParticleSystem>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "bomb")
+        {
+            Explode();
+            fire = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Explode()
     {
-        
+        aud.PlayOneShot(soundIn, Random.Range(1f, 5f));
+    }
+
+     private void Update()
+    {
+        if (fire)
+        {
+            firework.Play();
+        }
+
+        else if (!fire)
+        {
+            firework.Stop();
+        }
     }
 }
+
